6214891e617c698cc4a8cf4130fbb55f22a3a860
Baptiste Choquet <baptiste.choquet@pm.me>
2021-03-01 15:26:52 +0100

    Commit logo et header

--- php/header.php
+++ php/header.php
@@ -1,14 +1,17 @@
+<head>
+<link rel="stylesheet" href="../css/style.css">
+</head>
 <header>
         <div class="logo">
-          <img src="" alt="">
+          <img src="../img/logo-formation.png" alt="logo-formation">
         </div>
         <div class="nav-menu">
           <ul class="menu">
-            <li class="nav-links">A propos</li>
-            <li class="nav-links">Services</li>
-            <li class="nav-links">FAQ</li>
-            <li class="nav-links">Nous Contacter</li>
-            <li class="nav-links">Mon compte</li>
+            <li class="nav-links"><a href ="about.php">À propos</a></li>
+            <li class="nav-links"><a href = "services.php">Services</a></li>
+            <li class="nav-links"><a href = "faq.php">FAQ</a></li>
+            <li class="nav-links"><a href = "contact.php">Nous contacter</a></li>
+            <li class="nav-links"><a href = "account.php">Mon compte</a></li>
           </ul>
         </div>
     </header>
