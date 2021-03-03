75d792c9fece930bc46d2893033ca9326b13f134
luxxx <lucas.liger@edu.itescia.fr>
2021-03-03 11:45:09 +0100

    ajout hompage et son css

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="../img/LogoWoodUpW.png" type="image/png" sizes="16x16">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/index.css">
   <title>Wood'Up</title>
 </head>
 <body>
@@ -26,133 +27,155 @@
         </div>
 
 
+        <section class="about">
 
-        <div class="card-about">
 
-        </div>
+          <div class="about-img">
+            <img src="../img/mainpage_about_img.jpg" alt="forest image" width="40%" length="40%">
+          </div>
+          
 
-      </section>
-      
-      <section class="service">
 
-        <div class="service-txt">
+          <div class="card-about">
+            <h2>À PROPOS DE NOUS</h2>
+            <p></br></br></p>
+            <h1>WOOD'UP QU'EST-CE QUE C'EST ?</h1>
+            <p></br></br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit eius modi dolores nemo deleniti esse praesentium quisquam possimus iure, temporibus corporis quod nihil minus asperiores commodi ipsum. Corrupti, sint porro.</p>
+              <a href="about.php" id="about-button">EN SAVOIR PLUS</a>
+          </div>
 
-        </div>
+        </section>
+        
+        <section class="service">
 
-        <div class="card-service-container">
-
-          <div class="card-service">
-
-            <div id="coaching-txt">
-
-            </div>
-
-            <div id="coaching-img">
-
-            </div>
+          <div class="service-txt">
+            <h1>NOS SERVICES</h1>
+            <p></br></br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia accusantium atque quo consequuntur doloribus minus error quae mollitia dolorum cum soluta, adipisci eius saepe, magni velit tenetur! Soluta, earum porro!</p>
 
           </div>
 
-          <div class="card-service">
-            
-            <div id="formation-txt">
+          <div class="card-service-container">
+
+            <div class="card-service">
+
+              <div id="coaching-txt">
+                <h2>COACHING EN LIGNE</h2>
+                <p></br></br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus vero consectetur libero</br></p>
+                  <a href="services.php" id="services-button2">EN SAVOIR PLUS</a>
+              </div>
+
+              <div id="coaching-img">
+                <img src="../img/mainpage_coaching_img.jpg" alt="forest image" width="400" length="400">
+              </div>
 
             </div>
 
-            <div id="formation-img">
-
-            </div>
-
-          </div>
-
-        </div>
-
-      </section>
-
-      <section class="testimony">
-
-        <div class="testimony-txt">
-
-        </div>
-
-        <div class="testimony-carousel" id="1">
-
-          <div class="carousel-card">
-
-            <div class="carousel-card-img">
-
-            </div>
-
-            <div class="carousel-card-txt">
-
-            </div>
-
-          </div>
-
-          <div class="carousel-card" id="2">
-
-            <div class="carousel-card-img">
-
-            </div>
-
-            <div class="carousel-card-txt">
+            <div class="card-service">
               
-            </div>
+              <div id="formation-txt">
+                <h2>FORMATION </br>VIDÉO</h2>
+                <p></br></br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus vero consectetur libero</br></p>
+                  <a href="services.php" id="services-button2">EN SAVOIR PLUS</a>
+              </div>
 
-          </div>
-          <div class="carousel-card" id="3">
-
-            <div class="carousel-card-img">
-
-            </div>
-
-            <div class="carousel-card-txt">
-              
-            </div>
-
-          </div>
-
-        </div>
-
-      </section>
-
-      <section class="trainer">
-
-        <div class="trainer-txt">
-
-        </div>
-
-        <div class="trainer-card-container">
-
-          <div class="trainer-card">
-
-            <div class="trainer-card-img">
-
-            </div>
-
-            <div class="trainer-card-txt">
+              <div id="formation-img">
+                <img src="../img/mainpage_formation_img.jpg" alt="forest image" width="400" length="400">
+              </div>
 
             </div>
 
           </div>
 
-          <div class="trainer-card">
+        </section>
 
-            <div class="trainer-card-img">
+        <section class="testimony">
+
+          <div class="testimony-txt">
+            <h1>ILS NOUS ONT DÉJÀ FAIT CONFIANCE</h1>
+
+          </div>
+
+          <div class="testimony-carousel" id="1">
+
+            <div class="carousel-card">
+
+              <div class="carousel-card-img">
+
+              </div>
+
+              <div class="carousel-card-txt">
+
+              </div>
 
             </div>
 
-            <div class="trainer-card-txt">
-              
+            <div class="carousel-card" id="2">
+
+              <div class="carousel-card-img">
+
+              </div>
+
+              <div class="carousel-card-txt">
+                
+              </div>
+
+            </div>
+            <div class="carousel-card" id="3">
+
+              <div class="carousel-card-img">
+
+              </div>
+
+              <div class="carousel-card-txt">
+                
+              </div>
+
             </div>
 
           </div>
 
-        </div>
+        </section>
 
-      </section>
+        <section class="trainer">
 
-    </div>
+          <div class="trainer-txt">
+            <h1>NOS FORMATEURS</h1>
+            <p></br></br>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque facere, quos iusto eum sunt fugit dolorum labore provident in pariatur, eaque eligendi consequuntur. Illo hic molestias perspiciatis similique, quasi nihil.</p>
+          </div>
+
+          <div class="trainer-card-container">
+
+            <div class="trainer-card">
+
+              <div class="trainer-card-img">
+                <img src="../img/bg_forest.jpg" alt="forest image" width="35%" length="40%">
+              </div>
+
+              <div class="trainer-card-txt">
+                <h2>HÉLÈNE BOLLIOT</h2>
+                  <a href="contact.php" id="contact-button"> DÉCOUVRIR </a>
+              </div>
+
+            </div>
+
+            <div class="trainer-card">
+
+              <div class="trainer-card-img">
+                <img src="../img/bg_forest.jpg" alt="forest image" width="35%" length="40%">
+              </div>
+
+              <div class="trainer-card-txt">
+                <h2>CLÉMENT LACHAUD</h2>
+                  <a href="contact.php" id="contact-button"> DÉCOUVRIR </a>
+              </div>
+
+            </div>
+
+          </div>
+
+        </section>
+
+      </div>
 
     <?php include "php/footer.php"?>
 </body>
