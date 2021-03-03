652b6b4093bb8377145e8b2ca917a8fb313b6184
Emeric <papin.emeric@gmail.com>
2021-03-03 10:48:09 +0100

    Page FAQ finis

---
+++ FAQ.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wood'up</title>
+    <link rel="stylesheet" href="css/styleheader.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include "php/header.php" ?>
+    
+
+    <div class="who">
+        <h1>UNE QUESTION?</h1>
+        <h2>SI VOUS N'AVEZ PAS LA REPONSE A VOTRE QUESTION<br>
+        CONTACTEZ NOUS</h2>
+        <div class="ab"><a href="contact.php"style="color:white;">CONTACT</a></div>
+    </div>
+    <div class="questions">
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+            <div class="question-card"><h2>QUESTION</h2><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Quibusdam accusantium aliquid commodi earum beatae quo magnam<br> eligendi optio nihil perferendis, hic voluptates reprehenderit <br>voluptatem, accusamus veniam fugiat illo quas architecto.</p></div>
+    </div>
+    <?php include "php/footer.php" ?>
+</body>
+</html>
