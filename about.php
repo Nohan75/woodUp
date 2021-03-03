a133e369dc00ac7a85930ffa1225759c578204a6
luxxx <lucas.liger@edu.itescia.fr>
2021-03-03 14:48:56 +0100

    v1 finale

---
+++ about.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wood'up</title>
+    <link rel="stylesheet" href="css/styleabout.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include "php/header.php" ?>
+
+    <div class="who">
+        <h1>QUI SOMMES NOUS?</h1>
+    </div>
+        <div class="consulting">
+            <div class="consulting-txt">
+                <h2>WOOD'UP CONSULTING</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing</br>
+                     elit. Voluptatibus, id incidunt repellendus explicabo</br>
+                      maiores ab minima cum ut accusantium, amet accusamus! </br>
+                      Fugiat aspernatur minima numquam qui eaque vero commodi</br>
+                       cupiditate soluta modi, laborum quod explicabo officiis!</br>
+                        Nihil, repellendus animi? Voluptatum fuga, placeat voluptate</br>
+                         sint, nam perspiciatis iusto, magni tempore sed provident porro</br>
+                          velit! Est quas facilis dicta nesciunt praesentium.</br></br>
+                           Est at aliquid delectus, quis maxime sapiente nulla quaerat </br>
+                           doloribus possimus molestias, nam explicabo expedita reiciendis</br>
+                            cumque perspiciatis hic et tenetur, id in facilis! Molestias</br>
+                             deleniti esse, labore impedit, sint consequatur veritatis dolor</br>
+                               assumenda cum reiciendis, odio modi neque ipsum.</br> Voluptatum officia,
+                                corrupti id similique aliquam delectus explicabo aliquid optio sit</br>
+                                 quod a ullam adipisci sed hic? Necessitatibus sunt optio</br>
+                                  perferendis est odio temporibus corrupti dolore aspernatur,</br>
+                                   rem excepturi, soluta minima ab iure recusandae aliquid asperiores</br>
+                                    reprehenderit ea, consequuntur alias aliquam iusto adipisci ipsa?</br></br>
+                                     Itaque, a assumenda. Deserunt, consequatur ipsum nihil qui</br>
+                                      asperiores quaerat accusamus corrupti a iusto quas similique</br>
+                                       saepe necessitatibus, sapiente quos dignissimos veritatis dolorum</br>
+                                        consequuntur deleniti nisi sint aut perferendis dicta architecto!</br>
+                                         Eum quo provident ullam molestias! Dolorem beatae pariatur ad numquam</br>
+                                          sint explicabo dolores sequi, eligendi autem, eum optio laboriosam facere</br>
+                                           quas quaerat hic. Esse, nesciunt dicta!
+                </p><br>
+                <a href="#" id="contact-button">NOUS CONTACTER</a>
+
+            </div>
+            <div class="card-consulting">
+
+            <div class="card-consulting1">
+
+            </div>
+            <div class="card-consulting2">
+                
+            </div>
+        </div>
+        </div>
+
+    <div class="training">
+        <div class="training-card">
+
+        </div>
+        <div class="training-txt">
+            <h2>WOOD'UP FORMATION</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing</br>
+                elit. Voluptatibus, id incidunt repellendus explicabo</br>
+                 maiores ab minima cum ut accusantium, amet accusamus! </br>
+                 Fugiat aspernatur minima numquam qui eaque vero commodi</br>
+                  cupiditate soluta modi, laborum quod explicabo officiis!</br>
+                   Nihil, repellendus animi? Voluptatum fuga, placeat voluptate</br>
+                    sint, nam perspiciatis iusto, magni tempore sed provident porro</br>
+                     velit! Est quas facilis dicta nesciunt praesentium.</br></br>
+                      Est at aliquid delectus, quis maxime sapiente nulla quaerat </br>
+                      doloribus possimus molestias, nam explicabo expedita reiciendis</br>
+                       cumque perspiciatis hic et tenetur, id in facilis! Molestias</br>
+                        deleniti esse, labore impedit, sint consequatur veritatis dolor</br>
+                          assumenda cum reiciendis, odio modi neque ipsum.</br> Voluptatum officia,
+                           corrupti id similique aliquam delectus explicabo aliquid optio sit</br>
+                            quod a ullam adipisci sed hic? Necessitatibus sunt optio</br>
+                             perferendis est odio temporibus corrupti dolore aspernatur,</br>
+                              rem excepturi, soluta minima ab iure recusandae aliquid asperiores</br>
+                               reprehenderit ea, consequuntur alias aliquam iusto adipisci ipsa?</br></br>
+                                Itaque, a assumenda. Deserunt, consequatur ipsum nihil qui</br>
+                                 asperiores quaerat accusamus corrupti a iusto quas similique</br>
+                                  saepe necessitatibus, sapiente quos dignissimos veritatis dolorum</br>
+                                   consequuntur deleniti nisi sint aut perferendis dicta architecto!</br>
+                                    Eum quo provident ullam molestias! Dolorem beatae pariatur ad numquam</br>
+                                     sint explicabo dolores sequi, eligendi autem, eum optio laboriosam facere</br>
+                                      quas quaerat hic. Esse, nesciunt dicta!
+           </p><br>
+           <a href="#" id="contact-button">NOTRE OFFRE</a>
+        </div>
+    </div>
+    <div class="pro">
+        <h2>NOS PROFESSIONELS ET NOS FORMATEURS</h2>
+    </div>
+    <div class="helene">
+        <div class="helene-card">
+            
+        </div>
+        <div class="helene-txt">
+            <H2>Helene Bolliot</H2>
+            <p>Lorem ipsum dolor, sit amet consectetur
+                adipisicing elit. Adipisci</br> 
+                blanditiis aliquid nobis est
+                cupiditate maxime velit quia sed </br>
+                amet repellendus, autem commodi, ipsum maiores</br> 
+                 saepe dolorem natus 
+                dolores! Ipsa, esse?</p>
+        </div>
+    </div>
+    <div class="clement">
+        <div class="clement-card">
+            
+        </div>
+        <div class="clement-txt">
+            <h2>Clement Lachaud</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur
+                adipisicing elit. Adipisci</br> 
+                blanditiis aliquid nobis est
+                cupiditate maxime velit quia sed </br>
+                amet repellendus, autem commodi, ipsum maiores</br> 
+                 saepe dolorem natus 
+                dolores! Ipsa, esse?</p>
+        </div>
+    </div>
+    <?php include "php/footer.php" ?>
+</body>
+</html>
